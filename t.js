b3209c2d24b98e959deb899de973f0c7baa0dd33
Victor Almeida <victor.vhaa@hotmail.com>
2021-08-09 20:00:23 -0300

    Create t.js

---
+++ t.js
@@ -0,0 +1,16 @@
+(function(){
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '//vhaalmeida.github.io/t.css';
+    head.appendChild(link);
+  
+    /*var targetDiv = document.getElementById("event-header-simple").getElementsByClassName("names")[0].getElementsByClassName("text")[0];
+    
+    var node = document.createElement("SPAN");
+    var textnode = document.createTextNode("Bem-vinda, Céu");
+    node.appendChild(textnode);
+    
+    targetDiv.appendChild(node);*/
+})()
